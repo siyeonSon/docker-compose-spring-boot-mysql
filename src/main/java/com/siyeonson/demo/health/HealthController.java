e9295ddc6aeedc0fb8e2f159cee8c9ebf2bb9e22
siyeonSon <siyeon0209@naver.com>
2025-04-01 21:19:24 +0900

    feat: health check api

---
+++ src/main/java/com/siyeonson/demo/health/HealthController.java
@@ -0,0 +1,16 @@
+package com.siyeonson.demo.health;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HealthController {
+
+    @GetMapping({"/", "health"})
+    public String healthCheck() {
+        return "Application Health Good!";
+    }
+
+}
